ce1de7cd72f19b9ba211c442a7e97305fdfa5186
alisson.campos <alisson.campos@origoenergia.com.br>
2022-08-18 12:31:31 -0300

    Conteúdo da aula 1

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>Ops você se perdeu :0</h1>
+      <Link href="/">
+        voltar para home
+      </Link>
+    </div>
+  )
+}
